b24a2ef2d92739e393ea4e3dcddc387fd7446833
BNdiema <bryantndiema@gmail.com>
2023-04-25 16:31:55 -0700

    Fixed

--- _printf.c
+++ _printf.c
@@ -1,45 +1,50 @@
 #include <stdio.h>
-#include <stdarg.h> 
+#include <stdarg.h>
 #include "main.h"
 
+/**
+ * _printf - prints charactes, integers and string
+ * @format: contains what needs to be printed
+ * Return: printed charcters
+ */
 
 int _printf(const char *format, ...)
 {
 va_list list;
 int count = 0;
 
- va_start(list, format);
+va_start(list, format);
 
- while (*format != '\0')
+while (*format != '\0')
 {
-  if  (*format == '%')
-    {
-      format++;
-      switch (*format)
-	{
-	case 'c':
-	 _putchar(va_arg(list, int));
-	  count++;
-	  break;
-	case 's':
-	  count += printf("%s" , va_arg(list, const char *));
-	  count++;
-	  break;
-	default:
-	  _putchar('%');
-	  _putchar(*format);
-	  count += 2;
-	  break;
-	}
-      format++;
-    }
-  	else
-      }
-      _putchar(*format);
-      count++;
-      format++;
-
-    }
- va_end(list);
+if  (*format == '%')
+{
+format++;
+switch (*format)
+{
+case 'c':
+_putchar(va_arg(list, int));
+count++;
+break;
+case 's':
+count += printf("%s", va_arg(list, const char *));
+count++;
+break;
+default:
+_putchar('%');
+_putchar(*format);
+count += 2;
+break;
+}
+format++;
+}
+else
+{
+_putchar(*format);
+count++;
+format++;
+}
+}
+va_end(list);
 return (count);
 }
