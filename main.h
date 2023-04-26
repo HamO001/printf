84ced69e3f161401bd8b88602e871c9ce4b3f004
HamO001 <ndarugastiv9@gmail.com>
2023-04-25 17:26:09 -0700

    ooh my

--- main.h
+++ main.h
@@ -5,6 +5,19 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct convert - defines a structure for symbols and functions
+ *
+ * @sym: The operator
+ * @f: The function associated
+ */
+struct typedef struct changed
+{
+	char *sym;
+	int (*f)(va_list);
+}convert;
+
+
 int _putchar(char);
 int _printf(const char *format, ...);
 
