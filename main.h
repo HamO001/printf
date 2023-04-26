0d4922ccdba09072b86cb2e9d45d64db98de0853
BNdiema <bryantndiema@gmail.com>
2023-04-26 05:13:28 -0700

    created string _manuplatore.c: file containing functions to manipulate strings

--- main.h
+++ main.h
@@ -6,8 +6,34 @@
 #include <stdarg.h>
 
 
-int _putchar(char c);
+/* Function that produces output according to a formart */
 int _printf(const char *formart, ...);
 
+/* Helper function prototypes */
+int _putchar(char c);
+int _puts(char *str);
+int _strlen(char *s);
+int _write_char(char c);
+int _write_string(char *str);
+
+/* converstion specifires functions */
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(va+list list);
+
+/* number printer functiond */
+int  print_decimal(va_list list);
+int print_unsigned(va_list list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_hex_upper(va_list list);
+
+/* String manipilator functions */
+char *rev_string(char *s);
+
+/* custom data types */
+typedef unsigned int unit;
+typedef unsigned long int ulong;
+typedef unsigned long long int ullong;
 
 #endif
